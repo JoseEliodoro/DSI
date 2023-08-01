f3c23564d9245794e740150bf56849a2ab7a23d6
José Carlos <jose85751696@gmail.com>
2023-08-01 17:56:45 -0300

    Tela inicial

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'src/themes/color_schemes.g.dart';
+import 'src/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: lightColorScheme,
+        appBarTheme: const AppBarTheme(centerTitle: true),
+        scaffoldBackgroundColor: const Color.fromRGBO(10, 175, 158, 1),
+        
+      ),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      routes: {
+        //title: 'Heal Analytics'
+        //'/': (context) => const Home(),
+        '/': (context) => const HomePage(),
+      },
+    );
+  }
+}
