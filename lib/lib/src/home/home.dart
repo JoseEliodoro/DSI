0e9d18a0e4914c24c823c07cffbcb03ad42a59b8
Michael Douglas <86388737+Maicomxd@users.noreply.github.com>
2023-08-02 19:45:02 -0300

    Add files via upload

---
+++ lib/lib/src/home/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('title'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
