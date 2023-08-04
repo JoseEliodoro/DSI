bf89fd3512023d502e7146e39d02ae0001c92d82
Michael <maicomsv6@gmail.com>
2023-08-04 06:57:45 -0300

    Telas Novas

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         //title: 'Heal Analytics'
         //'/': (context) => const Home(),
       },
-      home: CadastroPage(),
+      home: HomePage(),
     );
   }
 }
