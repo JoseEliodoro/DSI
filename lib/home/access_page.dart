6469e84233c813bfa6754876df58e19b2274495e
José Carlos <jose85751696@gmail.com>
2023-08-19 12:41:08 -0300

    refacture webview

--- lib/home/access_page.dart
+++ lib/home/access_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:heal_anality/home/custom_drawer.dart';
-import 'package:heal_anality/widgets/buttons_menu.dart';
-import 'package:heal_anality/widgets/custom_appbar.dart';
+import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../services/auth_service.dart';
+import '../widgets/buttons_menu.dart';
+import '../widgets/custom_appbar.dart';
+import 'custom_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,7 +26,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    //AuthService auth = Provider.of<AuthService>(context);
+    AuthService auth = Provider.of<AuthService>(context);
+
     return Scaffold(
       drawer: const CustomDrawer(),
       appBar: customappbar(),
@@ -31,16 +35,21 @@ class _HomePageState extends State<HomePage> {
         children: [
           const ButtonsMenu(),
           Expanded(
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                double remainingHeight =
-                    MediaQuery.of(context).size.height - constraints.maxHeight;
-                return Container(
-                  height: remainingHeight,
-                  width: double.infinity,
-                  child: WebViewWidget(controller: controller),
-                );
-              },
+            child: WebViewWidget(
+              controller: WebViewController()
+                ..setJavaScriptMode(JavaScriptMode.unrestricted)
+                ..setNavigationDelegate(
+                  NavigationDelegate(
+                    onPageStarted: (String url) {
+                      print('começou');
+                    },
+                    onPageFinished: (String url) {
+                      print('Terminou');
+                    },
+                  ),
+                )
+                ..loadRequest(
+                    Uri.parse('https://unname.streamlit.app/?user=${auth.usuario?.uid}')),
             ),
           ),
         ],
