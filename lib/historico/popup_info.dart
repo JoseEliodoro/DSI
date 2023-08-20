4fb6a1d49ac17ac0b193d424b917eb491986c9f3
José Carlos <jose85751696@gmail.com>
2023-08-19 21:41:29 -0300

    Update popup_info.dart

--- lib/historico/popup_info.dart
+++ lib/historico/popup_info.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'models/pacient.dart';
 
 class PopUpInfo extends StatelessWidget {
-  final List<Pacient> tabela;
+  final List<Predict> tabela;
   final int i;
   const PopUpInfo({super.key, required this.tabela, required this.i});
 
